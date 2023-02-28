1f4eac6a70888ebfd6aca2b21c867410ce663fa6
Michal <michal.golemo98@gmail.com>
2023-02-28 03:40:47 +0100

    initial commit

--- app/main.c
+++ app/main.c
@@ -3,13 +3,7 @@
 
 int main(void)
 {
-    register const int x = 11;
-    register const int y = 5;
-
-    printf("%d + %d = %d\n", x, y, foo_add(x, y));
-    printf("%d - %d = %d\n", x, y, foo_sub(x, y));
-    printf("%d * %d = %d\n", x, y, foo_mul(x, y));
-    printf("%d / %d = %d\n", x, y, foo_div(x, y));
+    printf("Hello World\n");
 
     return 0;
 }
--- include/foo.h
+++ include/foo.h
@@ -1,9 +1,4 @@
 #ifndef FOO_H
 #define FOO_H
 
-int foo_add(int x, int y);
-int foo_sub(int x, int y);
-int foo_mul(int x, int y);
-int foo_div(int x, int y);
-
 #endif
